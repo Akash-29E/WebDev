5ac8f2bfdde9a382e5a581547e00a9204828577d
Akash-29E <parmaraakash45@gmail.com>
2025-07-17 10:50:04 -0400

    farmStand Project Complete

--- MongooseExpress/index.js
+++ MongooseExpress/index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Product = require('./models/product');
+const cats = Product.schema.obj.category.enum;
+// const cats = productSchema.obj.category.enum;
 const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
@@ -16,42 +18,53 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended:true}))
-app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
 
 app.get('/products', async (req, res) => {
+    const Cat = req.query.category ? req.query.category : 'All';
     const products = await Product.find({});
-    res.render('products/index', { products })
+    // Search by Query param if it assign in url
+    res.render('products/index', { products, cats, Cat })
 })
 
-app.get('/products/new', (req,res) => {
-    res.render('products/new');
+app.get('/products/new', (req, res) => {
+    const Cat = req.query.category ? req.query.category : 'vegetable';
+    res.render('products/new', { cats, Cat });
 })
 
-app.post('/products', async (req,res) =>{
+app.post('/products', async (req, res) => {
     console.log(req.body);
     const addNew = new Product(req.body);
     await addNew.save();
     res.redirect(`/products/${addNew._id}`)
 })
 
-app.get('/products/:id', async (req,res) => {
-    const {id} = req.params;
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/details', {product})
+    res.render('products/details', { product })
 })
 
-app.get('/products/:id/edit', async (req,res) => {
-    const {id} = req.params;
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit', {product});
+    res.render('products/edit', { product, cats });
 })
 
-app.put('/products/:id', async (req,res) =>{
-    const {id} = req.params;
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body);
-    console.log(req.body);
     res.redirect(`/products/${product._id}`)
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    console.log(product);
+    res.redirect('/products');
 })
 
 app.listen('3000', () => {
